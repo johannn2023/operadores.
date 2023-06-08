5d8ac45ddeff26d7c606ff0be9eb42389654baa5
johannn2023 <132423598+johannn2023@users.noreply.github.com>
2023-06-07 21:19:51 -0500

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,135 @@
+Base=float(input("Ingrese el valor de la base de su triagulo: "))
+altura=float(input("Ingrese el valor de la altura de su triagulo: "))
+area= Base * altura
+areatotal=area/2
+print("el area de su triangulo es: ",areatotal)
+
+print("ejercicio 2")
+
+num1=0
+num2=0
+suma=0
+print("bienvenido usuario")
+print("ingrese un numero entero")
+num1=int(input())
+print("ingrese otro numero:")
+num2=int(input())
+suma=num1+num2
+print("el resultado de la suma es ",suma)
+
+print("ejercicio 3")
+
+print("ingrese un numero")
+numeroy=int(input())
+potenciador=numeroy**2
+print("el cuadro es",potenciador)
+
+print("ejercicio 4")
+
+#Suma de 1234 + 532
+print("El primer numero es 1234")
+numero1=1234
+print("El segundo numero es 532")
+numero2=532
+s=numero1+numero2
+print("El resultado es",s)
+
+
+print("ejercicio 5")
+
+print("¿cuanto es 1234-532?")
+n1=1234
+print("se restan")
+n2=532
+resta=n1-n2
+print("el resultado es",resta)
+
+print("ejercicio 6")
+
+print("¿cuanto es 1234*532?")
+numero1=1234
+numero2=532
+multiplicacion=numero1+numero2
+print("el resultado es",multiplicacion)
+
+print("ejercicio 7")
+
+print("cuanto es 1234 divido entre 532")
+numero1=1234
+numero2=532
+D=numero1/numero2
+print("el resultado es ",D)
+
+print("ejercicio 8")
+
+print("modulo de 1234 entre 532")
+numero1=1234
+numero2=532
+P=numero1%numero2
+print("el resultado es ",P)
+
+print("ejercicio 9")
+
+print("¿cuantos euros quiere convertir en dolares?")
+numero1=int(input())
+numero2=float(1.10)
+euros=numero1*numero2
+print("el resultado es",euros)
+
+print("ejercicio 10")
+print("ingrese el largo de el rectangulo")
+numero1=float(input())
+print("ingrese el ancho del rectangulo")
+numero2=float(input())
+A=numero1*numero2
+print("el area es ",A)
+
+print("ejercicio 11")
+
+print("calculemos el area del cilindro")
+print("digite el radio de el cilindro")
+numero1=float(input())
+pi=3.1416
+p=numero1**2
+a2=pi*p
+print("el area es de",a2)
+print("digite la altura de el cilindro")
+numero2=float(input())
+altura=a2*n2
+print("el volumen es",altura)
+
+print("ejercicio 12")
+
+print("vamos a calcular el area de el cuadrado")
+print("ingrese el valor de un lado ")
+numero1=int(input())
+a=numero1*numero1
+print("el area de el cuadrado es",a)
+p=numero1+numero1+numero1+numero1
+print("el perimetro de el cuadrado es",p)
+
+print("ejercicio 13")
+
+print("digite el valor del circulo")
+numero1=float(input())
+ñ=numero1/1
+pi=3.14
+lon=pi*ñ
+l=p**2
+a=l*pi
+print("el  radio es de",ñ)
+print(  "la longitud de el circulo es",lon)
+print("el area es de",a)
+
+
+print("ejercicio 14")
+
+print("escriba su primer valor")
+n1=float(input())
+print("escriba su segundo valor")
+n2=float(input())
+print("escriba su tercer valor")
+n3=float(input())
+suma=n1+n2+n3
+divir=s/3
+print("El promedio es de",divir)
